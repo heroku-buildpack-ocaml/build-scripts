254a6b9a64d3a51406c5e56b3be6be14d108ef55
mzp <mzpppp@gmail.com>
2013-06-07 19:17:12 +0400

    add eliom

---
+++ eliom/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+vulcan build -p /app/vendor/ocamlbrew/opamlib/system -v -c ./setup
--- ocamlbrew/build.sh
+++ ocamlbrew/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 vulcan build -p /app/vendor/ocamlbrew -v -c \
-  'curl -kL https://raw.github.com/hcarty/ocamlbrew/master/ocamlbrew-install | env OCAMLBREW_BASE="/app/vendor/ocamlbrew" bash'
+  'curl -kL https://raw.github.com/hcarty/ocamlbrew/master/ocamlbrew-install | env OCAMLBREW_BASE="/app/vendor/ocamlbrew" OCAMLBREW_OPAMROOT="/app/vendor/ocamlbrew/opamlib" bash -x && rm -rf /app/vendor/ocamlbrew/ocaml-4.00.1/build'
